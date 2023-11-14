2ab0f613d45ab458b43d971da877f70e6e3b23af
boilermaker-ben <67593059+boilermaker-ben@users.noreply.github.com>
2023-11-14 15:25:05 -0500

    Update script.js
    Typo correction

--- script.js
+++ script.js
@@ -5651,7 +5651,7 @@ function dataTransfer(redirect,thursOnly) {
   const commentInclude = ss.getRangeByName('COMMENTS_PRESENT').getValue();
   let continueImport = false;
   if (redirect == null) {
-    let weekPrompt = ui.alert('IMPORT PICKS\r\n\r\nBring in picks from week ' + week + '?\r\n\r\nSelectiong \'NO\' will allow you to select a different week', ui.ButtonSet.YES_NO_CANCEL);
+    let weekPrompt = ui.alert('IMPORT PICKS\r\n\r\nBring in picks from week ' + week + '?\r\n\r\nSelecting \'NO\' will allow you to select a different week', ui.ButtonSet.YES_NO_CANCEL);
     if (weekPrompt == 'NO') {
       weekPrompt = ui.prompt('ENTER WEEK\r\n\r\nType the number of the week you\'d like to import:',ui.ButtonSet.OK_CANCEL);
       let regex = new RegExp(/[0-9]{1,2}/);
